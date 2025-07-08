33a268de30e77305f16eb26b9839dbf99e0d5265
pierre-charlesworth <pierre.charlesworth@gmail.com>
2025-07-08 10:58:12 +0200

    Version 0.13
    Attempt to resolve service worker issue

--- service-worker.js
+++ service-worker.js
@@ -40,6 +40,11 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  // We only handle GET requests, POSTs, etc. should not be cached.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
@@ -50,17 +55,16 @@ self.addEventListener('fetch', event => {
 
         return fetch(event.request).then(
           networkResponse => {
-            // Check if we received a valid response
-            if(!networkResponse || networkResponse.status !== 200) {
+            // We don't cache opaque responses or error responses.
+            // .ok is true for status codes in the range 200-299.
+            if (!networkResponse || !networkResponse.ok || networkResponse.type === 'opaque') {
               return networkResponse;
             }
 
-            // We don't cache from non-'basic' types (e.g. CDN requests)
-            // as they are opaque responses and can fill up storage.
-            if(networkResponse.type !== 'basic') {
-              return networkResponse;
-            }
-
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
             const responseToCache = networkResponse.clone();
 
             caches.open(CACHE_NAME)
