32a2189bf09b122450eee619456bc178bb707ccf
pierre-charlesworth <pierre.charlesworth@gmail.com>
2025-07-08 12:57:01 +0200

    Version 0.17

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [
+        react(),
+        VitePWA({
+          registerType: 'autoUpdate',
+          manifest: {
+            short_name: 'OD600 Calc',
+            name: 'OD600 Dilution Calculator',
+            start_url: '.',
+            display: 'standalone',
+            theme_color: '#1e293b',
+            background_color: '#0f172a'
+          }
+        })
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
